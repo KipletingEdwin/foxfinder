5abfa5388590f175b7862b765da4af39b3de8f32
Kipleting <kipletingedwin4@gmail.com>
2022-07-19 01:10:26 +0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+/* 
+Deliverables
+- Use the `useState` hook for the `image` and `likes` variables
+- When the New Fox button is clicked, use the `handleNewFoxClick` function to fetch a random fox image. Use the image response from fetch to set the image variable.
+- When the Like button is clicked, create an event handler that will increment the number of likes in state. Use the callback version of the state setter function to set the new number of likes.
+*/
 
 function App() {
+  const image = "https://randomfox.ca/images/41.jpg"
+  const likes = 0
+  
+  function handleNewFoxClick() {
+    fetch("https://randomfox.ca/floof/")
+      .then((response) => response.json())
+      .then(({ image }) => {
+        console.log(image)
+      })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🦊 FoxFindr 🦊</h1>
+      <div className="buttons">
+          <button>New 🦊 Please</button>
+          <button>Likes: {likes}</button>
+      </div>
+      <img src={image} alt="Random Fox" />
     </div>
   );
 }
 
-export default App;
+export default App
